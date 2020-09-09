771edf56a00de2a95a7054641ea856e45afa15f4
MrTetris-rv <sharailee@bk.ru>
2020-09-09 20:00:49 +0300

    Changing fonts, adding navmesh cat, postprocessing

---
+++ Quest/Assets/Scripts/CatAI/NPCpatrol.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCpatrol : MonoBehaviour
+{
+
+    [SerializeField]
+    bool _patrolWaiting;
+    [SerializeField]
+    float _totalWaitTime = 3f;
+    [SerializeField]
+    float _switchProbability = 0.2f;
+    [SerializeField]
+    List<WayPoint> _patrolPoints = new List<WayPoint>();
+    [SerializeField]
+    Animator anim;
+    NavMeshAgent _navMeshAgent;
+    int _currentPatrolIndex;
+    bool _travelling;
+    bool _waiting;
+    bool _patrolForward;
+    float _waitTimer;
+
+
+    private void Start()
+    {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+        if (_navMeshAgent == null)
+        {
+            Debug.LogError("The nav mesh agent component isn't attached to " + gameObject.name);
+        }
+        else if(_patrolPoints != null && _patrolPoints.Count >= 2)
+        {
+            _currentPatrolIndex = 0;
+            SetDestination();
+        }
+        else
+        {
+            Debug.Log("Insufficient patrol points for basic patrolling behaviour");
+        }
+    }
+
+    private void Update()
+    {
+        if(_travelling && _navMeshAgent.remainingDistance <= 1.0f)
+        {
+            _travelling = false;
+           
+            if (_patrolWaiting)
+            {
+                _waiting = true;
+                _waitTimer = 0f;
+            }
+            else
+            {
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+        if (_waiting)
+        {
+            _waitTimer += Time.deltaTime;
+            anim.SetBool("isWalking", false);
+            if (_waitTimer >= _totalWaitTime)
+            {            
+                ChangePatrolPoint();
+                SetDestination();
+            }
+        }
+
+    }
+
+    private void SetDestination()
+    {
+        if(_patrolPoints != null)
+        {
+            Vector3 targetVector = _patrolPoints[_currentPatrolIndex].transform.position;
+            _navMeshAgent.SetDestination(targetVector);
+            _travelling = true;
+            anim.SetBool("isWalking", true);
+        }
+    }
+    private void ChangePatrolPoint()
+    {
+        if(Random.Range(0f,1f)<= _switchProbability)
+        {
+            _patrolForward = !_patrolForward;
+        }
+        if (_patrolForward)
+        {
+            _currentPatrolIndex = (_currentPatrolIndex + 1) % _patrolPoints.Count;
+        }
+        else
+        {
+            if(--_currentPatrolIndex < 0)
+            {
+                _currentPatrolIndex = _patrolPoints.Count - 1;
+            }
+        }
+    }
+}
---
+++ Quest/Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject optionScreen;
+    [SerializeField]
+    public GameObject menuScreen;
+
+
+    public void Options()
+    {
+        menuScreen.SetActive(false);
+        optionScreen.SetActive(true);
+    }
+
+    public void Back()
+    {
+        menuScreen.SetActive(true);
+        optionScreen.SetActive(false);
+    }
+
+    public void Continue()
+    {
+
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ExitApp()
+    {
+        Application.Quit();
+    }
+}
